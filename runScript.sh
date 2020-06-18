4186f6be745fd7af8ee5ead77882cb43d24a7d1f
John Whitman <whitman.103@osu.edu>
2020-06-18 01:12:53 -0400

    Update runScript.sh

--- runScript.sh
+++ runScript.sh
@@ -1,7 +1,6 @@
-#!bin/bash
-#SBATCH --nodes=15
+#!/bin/bash
+#SBATCH --ntasks=15
 
 module load gcc/7.2.0
 module load openmpi-1.8/gcc
-mpic++ -O2 -g MPIGillespie.cpp fuzzyDef.cpp GillespieFunctions.cpp -o MPIGillespie.exe
 mpirun -np 15 ./MPIGillespie.exe
