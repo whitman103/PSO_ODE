8808d85b0da600641edaa07057f0db1c7f947876
John Whitman <whitman.103@osu.edu>
2020-06-22 17:04:34 -0400

    Update MPIGillespieTest.cpp

--- MPIGillespieTest.cpp
+++ MPIGillespieTest.cpp
@@ -54,7 +54,7 @@ int main(int argc, char* argv[]){
 	
 	string masterFolder="MasterFolder";
 	mkdir(masterFolder.c_str(),S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-	masterFolder+="//";
+	masterFolder+="\\\\";
 
 	string baseHillStructure("randomHillStructure");
 	
@@ -231,7 +231,7 @@ int main(int argc, char* argv[]){
 	
 	for(int set=0;set<numOfSets;set++){
 		string currentFolder=masterFolder+"outputSet_"+to_string(set);
-		currentFolder+="\\";
+		currentFolder+="\\\\";
 		mkdir(currentFolder.c_str(),S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
 		ofstream fitnessValuesOut(currentFolder+"fitnessMasterOut_.txt");
